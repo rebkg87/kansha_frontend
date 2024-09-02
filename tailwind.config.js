6082012074ed32d52fcb9903d6791059f2d5b555
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-02 14:50:18 +0200

    add initial tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-green": "#213D33",
+        "medium-green":"#36AA8E",
+        "medium-soft-green":"#6AC3A4",
+        "soft-green":"#AAFFEB",
+        "super-light-green":"#DCFEF2",
+        "gray":"#747474",
+        "light-green":"#d9d9d9",
+        "mood-angry":"#FF0000",
+        "mood-sad":"#FFA600",
+        "mood-neutral":"#FAEC29",
+        "mood-happy":"#1FA455",
+        "mood-very-happy":"#9ECB4A"
+      } 
+    },
   },
   plugins: [],
 }
