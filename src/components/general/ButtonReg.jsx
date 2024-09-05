838a99e14a6ccd67465156488fb026f60f5d5b64
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-05 02:00:02 +0200

    add buttons login components

--- src/components/general/ButtonReg.jsx
+++ src/components/general/ButtonReg.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
-export const ButtonReg = () => {
+import React from "react";
+const ButtonReg = ({ onClick, buttonText, buttonStyle, iconButton, label }) => {
     return (
-        <div>
-            <button/>
+        <div className="">
+            <button
+                onClick={onClick}
+                className={`${buttonStyle} w-full h-[54px] text-h6 font-bold font-barlow rounded-[10px]`} >
+                <img src={iconButton} alt={label} />
+                {buttonText}
+            </button>
         </div>
     )
 }
+
+export default ButtonReg;
