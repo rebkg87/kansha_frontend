67d660a75cf860cf6ee2f4940615f34c3ea59c08
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-05 14:29:13 +0200

    add affirmation card component

---
+++ src/components/profile/AffirmationCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AffirmationCard = () => {
+    return (
+        <div className='w-full h-[171px] border-2 border-soft-green rounded-[20px] shadow-inner-inputV2 bg-medium-soft-green flex flex-col justify-center items-center p-8'>
+            <h6 className=' text-h6 font-barlow font-bold text-dark-green mb-2'>Afirmación Diaria</h6>
+            <h5 className='text-h5 font-barlow font-bold text-soft-green text-center leading-h5'>“Tengo todo lo que necesito para hacer de hoy un gran día.”</h5>
+        
+        </div>
+    )
+}
