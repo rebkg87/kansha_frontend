903d0c035b063c3d0767fc6b49e83f9f1d9de678
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-06 00:05:17 +0200

    add general components

--- src/components/general/ButtonReg.jsx
+++ src/components/general/ButtonReg.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-const ButtonReg = ({ onClick, buttonText, buttonStyle, iconButton, label }) => {
+const ButtonReg = ({ onClick, buttonText, buttonStyle, imgButtonStyle, iconButton, label }) => {
     return (
-        <div className="">
+        <div className="flex justify-between">
             <button
                 onClick={onClick}
-                className={`${buttonStyle} w-full h-[54px] text-h6 font-bold font-barlow rounded-[10px]`} >
-                <img src={iconButton} alt={label} />
+                className={`${buttonStyle} w-full h-[54px] text-h6 font-bold font-barlow rounded-[10px] flex gap-1 justify-center items-center`} >
                 {buttonText}
+                <img className={imgButtonStyle} src={iconButton} alt={label} />
             </button>
         </div>
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,16 @@ import InputV3 from "../components/general/InputV3"
 import { AffirmationCard } from "../components/profile/AffirmationCard"
 import userIcon from '../../public/assets/images/icons/profile.svg';
 import { Greeting } from "../components/profile/Greeting";
+import NavbarBottonButton from "../components/userLayout/NavbarBottonButton";
+import homeIcon from '../../public/assets/images/icons/home.svg'
+import SmallButton from "../components/general/SmallButton"
+import googleIcon from '../../public/assets/images/icons/icons8-logo-de-google.svg'
 
 const Home = ({ }) => {
     return (
-        <div className=" flex flex-col p-11 gap-4">
-            <ButtonReg type="button" buttonStyle='bg-medium-soft-green text-white' buttonText="Iniciar Sesión" />
-            <ButtonReg type="button" buttonStyle='bg-white text-gray border-2 border-soft-green' buttonText="Iniciar Sesión con Google" />
+        <div className=" flex flex-col p-11 gap-3">
+            <ButtonReg type="button" buttonStyle='bg-medium-soft-green text-white' buttonText="Iniciar Sesión" imgButtonStyle="hidden" />
+            <ButtonReg type="button" buttonStyle='bg-white text-gray border-2 border-soft-green' buttonText="Iniciar Sesión con Google" iconButton={googleIcon} imgButtonStyle='h-[20px] w-[20px]' />
 
             <InputV1 placeholder={"Email"} />
             <AffirmationCard />
@@ -22,6 +26,7 @@ const Home = ({ }) => {
             />
             <Greeting greeting={"Buenos días"} name={"Rebeca"}
             />
+            <SmallButton buttonStyle='w-[95px] h-[45px] bg-medium-green text-super-light-green' buttonText="Guardar"/>
 
         </div>
     )
