62057084cd5e4023588b8409792427e12bd10ec6
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-09 20:30:16 +0200

    calendar components styles

--- src/components/profile/DayCard.jsx
+++ src/components/profile/DayCard.jsx
@@ -7,9 +7,9 @@ const DayCard = ({ dayWeek, dayNumber, isToday }) => {
         rounded-lg mx-1 transition-all duration-150 cursor-pointer justify-center p-2`}
         >
             {isToday && (
-                <span className="flex h-3 w-3 absolute -bottom-1 -right-1">
+                <span className="flex justify-center items-center h-2 w-2 absolute -bottom-1 ">
                     <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-medium-soft-green opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-medium-soft-green"></span>
+                    <span className="relative inline-flex rounded-full h-2 w-2 bg-medium-soft-green"></span>
                 </span>
             )}
             <div className="flex items-center">
