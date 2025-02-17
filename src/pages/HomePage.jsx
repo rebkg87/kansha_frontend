1d2c17553667e93585adff1d2daadc31d33e816f
Rebeca Garcia <rebe.ga87@gmail.com>
2025-02-17 15:02:16 +0100

    update project

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import WeekCalendar from "../components/home/WeekCalendar"
 import AddGratitudeBtn from "../components/home/AddGratitudeBtn"
 import { useNavigate } from "react-router-dom"
 import useGet from "../customHooks/useGet"
+import { AnimatePresence, motion } from "framer-motion"
 
 const Home = ({ }) => {
 
@@ -11,20 +12,28 @@ const Home = ({ }) => {
 
     const { data: dataUser } = useGet(`/users/user`)
     console.log(dataUser);
-    
+
     const handleAddGratitudeClick = () => {
         navigate('/user/gratitude')
     }
 
     return (
+
         <div className="flex flex-col">
-            <div className="p-11">
-                <AffirmationCard />
-            </div>
+            <AnimatePresence>
+                <motion.div
+                    initial={{ scale: 1 }}
+                    whileHover={{ scale: 1.05 }}>
+                    <div className="p-11">
+                        <AffirmationCard />
+                    </div>
+                </motion.div>
+            </AnimatePresence>
             <div className='mt-[-20px] ml-11 mb-5'>
                 <AddGratitudeBtn onClick={handleAddGratitudeClick} />
             </div>
         </div>
+
     )
 }
 
